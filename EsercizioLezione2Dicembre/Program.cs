459cfa840c6d4c5485ad03c467aca0ecf413a0ac
tommaso lauria <tommaso.lauria9@gmail.com>
2020-12-02 12:23:19 +0100

    Add project files.

---
+++ EsercizioLezione2Dicembre/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibreriaEs2_12;
+
+namespace EsercizioLezione2Dicembre
+{
+    public class Program
+    {
+         static void Main(string[] args)
+        {
+            uint a = PariDispariIntervalo.LeggiIntero();
+            uint b = PariDispariIntervalo.LeggiIntero();
+            PariDispariIntervalo.PariDispari(a, b);
+        }
+        
+    }
+}
---
+++ LibreriaEs2_12/PariDispariIntervalo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LibreriaEs2_12
+{
+    public class PariDispariIntervalo
+    {
+        public static uint LeggiIntero()
+        {
+            Console.Write("inserisci un numero intero positivo: ");
+            uint a = uint.Parse(Console.ReadLine());
+            return a;
+        }
+        public static void PariDispari(uint a, uint b)
+        {
+            int countP = 0, countD = 0;
+            uint max = Math.Max(a, b);
+            uint min = Math.Min(a, b);
+            for (uint i = min; i <= max; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i}-pari;");
+                    countP++;
+                }
+                else
+                {
+                    Console.WriteLine($"{i}-dipari;");
+                    countD++;
+                }
+            }
+            Console.WriteLine($"i numeri pari sono: {countP};\ni numeri dispari: {countD}.");
+        }
+    }
+}
